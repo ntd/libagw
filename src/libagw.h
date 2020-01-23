501efce31d0966b7d49d63cb690a1d7eebeaffe6
Nicola Fontana <ntd@entidi.it>
2020-01-23 15:00:17 +0100

    Add main header file

---
+++ src/libagw.h
@@ -0,0 +1,24 @@
+/* libagw - Additional GTK Widgets
+ * Copyright (C) 2020  Nicola Fontana <ntd@entidi.it>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __LIBAGW_H__
+#define __LIBAGW_H__
+
+#include "agw-gauge.h"
+
+#endif /* __LIBAGW_H__ */
